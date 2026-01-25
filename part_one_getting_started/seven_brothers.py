3885f8ad41287afe73fed9a7850c824536344b0e
Mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-25 09:17:36 -0500

    starter excercises done

---
+++ part_one_getting_started/seven_brothers.py
@@ -0,0 +1,21 @@
+"""
+The program below is suppose dto print out the name of brothers in alphabetical order, 
+but its not working quite right please fix so correct order.
+
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")
+
+"""
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Tuomas")
+print("Timo")
