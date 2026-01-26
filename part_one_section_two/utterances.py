2cf1b2919d724594b20ed9c78600e3938e95d3f4
Mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-26 14:22:47 -0500

    changed name to folders for more clear progression, along with a readme detailing the project

---
+++ part_one_section_two/utterances.py
@@ -0,0 +1,24 @@
+"""
+Here is a program which should ask for three utteraance and print them out
+
+The 1st part: hickory
+The 2nd part: dickory
+The 3rd part: dock
+hickory-dickory-dock!
+
+# Fix the code
+part = input("The 1st part: ")
+part = input("The 1st part: ")
+part = input("The 1st part: ")
+print(part + part + part)
+
+
+"""
+
+
+part_one = input("The 1st part:")
+part_two = input("The 2nd part:")
+part_three = input("The 3rd part:")
+
+print(f"{part_one}-{part_two}-{part_three}!")
+
