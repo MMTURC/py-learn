a58e9260d468b2f057173f81973e3375e0d10c92
Mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-26 14:23:44 -0500

    Created readme

--- part_one_user_input/name_and_address.py
+++
@@ -1,8 +0,0 @@
-name = input("Given Name:")
-family_name = input("Family name:")
-address = input("Street address:")
-city_zip = input("City and postal code:")
-
-print(name + "" +  family_name)
-print(address)
-print(city_zip)
