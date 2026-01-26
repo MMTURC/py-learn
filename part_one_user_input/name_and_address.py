e48b8d79223196fdf6d7ea9c56078428ec53170f
Mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-25 22:17:19 -0500

    Moved on to user input and completed some excercises

---
+++ part_one_user_input/name_and_address.py
@@ -0,0 +1,8 @@
+name = input("Given Name:")
+family_name = input("Family name:")
+address = input("Street address:")
+city_zip = input("City and postal code:")
+
+print(name + "" +  family_name)
+print(address)
+print(city_zip)
