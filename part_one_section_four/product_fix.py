d2727e71dfcd0793a81ca7488f59c5a35898ddb7
Mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-28 21:02:54 -0500

    More excercises done

---
+++ part_one_section_four/product_fix.py
@@ -0,0 +1,26 @@
+"""
+
+This program asks the user for three numbers. The program then prints out their product, that is, 
+the numbers multiplied by each other. There is, however, something wrong with the program - it doesn't
+work quite right, as you can see if you run it.
+
+An example of the expected execution of the program:
+
+Sample output
+
+Please type in the first number: 2
+Please type in the second number: 3
+Please type in the third number: 5
+The product is 30
+
+
+"""
+
+
+number_one = int(input("Please type in the first number: "))
+number_two = int(input("Please type in the second number: "))
+number_three = int(input("Please type in the third number: "))
+
+product = number_one * number_two * number_three
+
+print("The product is", product)
---
+++ part_one_section_four/seconds_to_day.py
@@ -0,0 +1,13 @@
+"""
+
+Please write a program which asks the user for a number of days. The program then prints out 
+the number of seconds in the amount of days given.
+
+
+"""
+number_of_days = int(input("How many days"))
+
+days_in_seconds = number_of_days * 86400
+
+print(f"Seconds in that many days: {days_in_seconds}")
+
